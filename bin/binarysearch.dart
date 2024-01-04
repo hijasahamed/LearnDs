5cef14ad7aa846072a62052adbad24fcbc5baf46
hijas <hijasahamed2000@gmail.com>
2024-01-04 16:32:41 +0530

    last

---
+++ bin/binarysearch.dart
@@ -0,0 +1,42 @@
+ /*binarysearch(List arr, int target) {-
+  int l = 0;
+  int r = arr.length - 1;
+  while (l < r) {
+    int rounded = (l + r) ~/ 2;
+    // int rounded = middleindex.floor();
+
+    if (target == arr[rounded]) {
+      return rounded;
+    }
+    if (target < arr[rounded]) {
+      r = rounded - 1;
+    } else {
+      l = rounded + 1;
+    }
+  }
+}*/
+
+binaryChecking(List arr,int target){
+  int l=0;
+  int r=arr.length-1;
+  while(l<r){
+    int mid=(l+r)~/2;
+    if(target==arr[mid]){
+      print('target found in index $mid');
+      return;
+    }
+    if(target < arr[mid]){
+      r=mid-1;
+    }
+    else{
+      l=mid+1;
+    }
+  }
+  print('Target not found in the list');
+}
+
+main(){
+  List arr=[5,10,15,20,25,30,35,40];
+ var res=binaryChecking(arr, 30);
+ print(res);
+}
--- bin/learn_datastucture.dart
+++ bin/learn_datastucture.dart
@@ -1,23 +1,24 @@
-int binarysearch(List arr, int target) {
-  int l = 0;
-  int r = arr.length - 1;
-  while (l <= r) {
-    double middleindex = (l + r) / 2;
-    int rounded = middleindex.floor();
+main(){
+  List arr=[10,20,30,40,50,60,70];
+  binarysearch(arr, 10);
+}
 
-    if (target == arr[rounded]) {
-      return rounded;
+binarysearch(List arr,int target){
+  int l=0;
+  int r=arr.length-1; 
+  while(l<r){
+    int min=(l+r)~/2;
+    if(target==arr[min]){
+      print('Target found at index $min');
+      return;
     }
-    if (target < arr[rounded]) {
-      r = rounded - 1;
-    } else {
-      l = rounded + 1;
+    else if(target < arr[min]){
+      r=min-1;
+    }
+    else{
+      l=min+1;
     }
   }
-  return -1;
+  // print('No target found in the list');
 }
 
-main(){
-  List a=[5,10,15,20,25,30,35,40];
-  binarysearch(a, 25);
-}
