471db6c860a053906a669acccf0e87956867ba4e
hijas <hijasahamed2000@gmail.com>
2024-02-12 09:22:45 +0530

    last push

--- bin/learn_datastucture.dart
+++ bin/learn_datastucture.dart
@@ -4,62 +4,45 @@ class Node{
   Node? right;
   Node(this.data);
 }
-
 Node? root;
 
 insert(data){
   Node newnode=Node(data);
-  Node? currentroot=root;
+  Node? currentRoot=root;
   if(root==null){
     root=newnode;
     return;
   }
   while(true){
-    if(data<currentroot?.data){
-     if(currentroot?.left==null){
-      currentroot?.left=newnode;
-      return;
-     }
-     else{
-      currentroot=currentroot?.left;
-     }
-    }
-    else{
-      if(currentroot?.right==null){
-        currentroot?.right=newnode;
+    if(data<currentRoot?.data){
+      if(currentRoot?.left==null){
+        currentRoot?.left=newnode;
         return;
       }
       else{
-        currentroot=currentroot?.right;
+        currentRoot=currentRoot?.left;
+      }
+    }
+    else{
+      if(currentRoot?.right==null){
+        currentRoot?.right=newnode;
+        return;
+      }
+      else{
+        currentRoot=currentRoot?.right;
       }
     }
   }
 }
 
-// contains(root,data){
-//   Node? currentnode=root;
-//     if(data == currentnode?.data){
-//       return true;
-//     }
-//     else if(data < currentnode?.data){
-//       currentnode=currentnode?.left;
-//       contains(currentnode,data);
-//     }
-//     else{
-//       currentnode=currentnode?.right;
-//       contains(currentnode,data);
-//     }
-//   return false;
-// }
-
-  contains(data){
-  Node? currentnode=root;
-  while(currentnode !=null){
-    if(data <currentnode.data){
-      currentnode=currentnode.left;
+contains(data){
+  Node? currentroot=root;
+  while(currentroot!=null){
+    if(data<currentroot.data){
+      currentroot=currentroot.left;
     }
-    else if(data > currentnode.data){
-      currentnode=currentnode.right;
+    else if(data>currentroot.data){
+      currentroot=currentroot.right;
     }
     else{
       return true;
@@ -68,10 +51,61 @@ insert(data){
   return false;
 }
 
+inorder(Node? node){
+  if(node!=null){
+    inorder(node.left);
+    print(node.data);
+    inorder(node.right);
+  }
+}
+
+preorder(Node? node){
+  if(node!=null){
+    print(node.data);
+    preorder(node.left);
+    preorder(node.right);
+  }
+}
+
+postorder(Node? node){
+  if(node!=null){
+    postorder(node.left);
+    postorder(node.right);
+    print(node.data);
+  }
+}
+
+delete(data,currentroot){
+  while(currentroot != null){
+    if(data < currentroot.data){
+      currentroot=currentroot.left;
+    }
+    else if(data > currentroot.data){
+      currentroot=currentroot.right;
+    }
+    else{
+      if(currentroot.left!=null && currentroot.right!=null){
+        currentroot.data=minvalue(currentroot.right);
+        delete(currentroot.data,currentroot.right);
+      }
+    }
+  }
+}
+
+minvalue(current){
+  if(current==null){
+    return current;
+  }
+  else{
+    return minvalue(current.left);
+  }
+}
+
+
 main(){
   insert(10);
   insert(5);
-  insert(20);
+  insert(2);
+  insert(15);
   insert(30);
-  print(contains(30));
 }
---
+++ bin/trie.dart
@@ -0,0 +1,91 @@
+class TrieNode {
+  Map<String, TrieNode> children = {};
+  bool last = false;
+}
+
+class Trie {
+  TrieNode root = TrieNode();
+
+  insert(String data) {
+    TrieNode current = root;
+    for (int i = 0; i < data.length; i++) {
+      String char = data[i];
+      current.children.putIfAbsent(char, () => TrieNode());
+      current = current.children[char]!;
+    }
+    current.last = true;
+  }
+
+  insertsuffix(String data) {
+    for (int i = 0; i < data.length; i++) {
+      suffixhelper(i, data);
+    }
+  }
+
+  suffixhelper(int index, data) {
+    TrieNode current = root;
+    for (int i = index; i < data.length; i++) {
+      String char = data[i];
+      current.children.putIfAbsent(char, () => TrieNode());
+      current = current.children[char]!;
+    }
+    current.last = true;
+  }
+
+  search(String data) {
+    TrieNode current = root;
+    for (String char in data.split('')) {
+      if (!current.children.containsKey(char)) {
+        return false;
+      }
+      current = current.children[char]!;
+    }
+    return current.last;
+  }
+
+  startwith(String data) {
+    TrieNode current = root;
+
+    for (String char in data.split('')) {
+      if (!current.children.containsKey(char)) {
+        return false;
+      }
+      current = current.children[char]!;
+    }
+    return true;
+  }
+
+  prefixsearch(String data) {
+    TrieNode current = root;
+    List<String> result = [];
+    for (String char in data.split('')) {
+      if (!current.children.containsKey(char)) {
+        return result;
+      }
+      current = current.children[char]!;
+    }
+    prehelper(current, data, result);
+    return result;
+  }
+
+  prehelper(TrieNode current, String data, List<String> result) {
+    if (current.last) {
+      result.add(data);
+    }
+    for (String char in current.children.keys) {
+      prehelper(current.children[char]!, data + char, result);
+    }
+  }
+}
+
+void main() {
+  Trie trie = Trie();
+  trie.insertsuffix("apple");
+  trie.insertsuffix('cat');
+  // print(trie.search("apple"));
+  // print(trie.search("app"));
+  // trie.insert("app");
+  // print(trie.search("app"));
+  print(trie.prefixsearch("ca"));
+  print(trie.prefixsearch("ap"));
+}
